6e7ffbf3db8739ba819078af14294d185ac93f2e
SaradhaRam <saradhawork@gmail.com>
2021-09-09 21:32:53 -0500

    database created

--- deploy/app.py
+++ deploy/app.py
@@ -1,12 +1,16 @@
 # import necessary libraries
 from models import create_classes
 import os
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+import sqlite3
+from flask import Flask, redirect, render_template,url_for, request
+
+# from flask import (
+#     Flask,
+#     render_template,
+#     jsonify,
+#     request,
+#     redirect,
+#     url_for)
 
 #################################################
 # Flask Setup
@@ -17,25 +21,27 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+# from flask_sqlalchemy import SQLAlchemy
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///app.db"
 
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# # Remove tracking modifications
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-Patient = create_classes(db)
+# Patient = create_classes(db)
 
 # create route that renders index.html template
 @app.route("/")
 def home():
+    print ("inside route /")
     return render_template("index.html")
 
 
 # Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
+@app.route("/send", methods=["POST","GET" ])
 def send():
+    print("inside route /send")
     if request.method == "POST":
         age = request.form["age"]
         sex = request.form["sex"]
@@ -50,21 +56,55 @@ def send():
         slope = request.form["slope"]
         ca = request.form["ca"]
 
-        patient = Patient(age=age, sex = sex, cp = cp,trestbps=trestbps,
-        chol=chol,fbs=fbs,restecg=restecg,thalach=thalach,exang=exang,oldpeak=oldpeak,slope=slope,ca=ca)
-        db.session.add(patient)
-        db.session.commit()
-        return redirect("/api/patients")
+        # patient = Patient(age=age, sex = sex, cp = cp,trestbps=trestbps,
+        # chol=chol,fbs=fbs,restecg=restecg,thalach=thalach,exang=exang,oldpeak=oldpeak,slope=slope,ca=ca)
+        # db.session.add(patient)
+        # db.session.commit()
 
-    return render_template("form.html")
+          # call the method to store the data in database(sqlite)
+        store_patient(age, sex, cp, trestbps,chol, fbs, restecg, thalach, exang, oldpeak, slope, ca)
+
+        # return redirect(url_for('patients'))
+        return redirect('/api/patients')
+    return render_template('form.html')
+        # return redirect(url_for('/'))
+   
+
+
+        # return render_template("form.html")
 
 
 @app.route("/api/patients")
 def patients():
-    results = db.session.query(Patient.age).all()
+    print("inside route /api/patients")
+    # results = db.session.query(Patient.age).all()
+    
 
     return 'yoyoy'
 
 
+
+################## Function ##########
+
+def store_patient(age, sex, cp, trestbps,chol, fbs, restecg, thalach, exang, oldpeak, slope, ca):
+    print("hello")
+    connection = sqlite3.connect("C:/Users/vijay/Documents/Saradha_R/project3/project3/deploy/app.db") 
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    insert into patients
+    values (?,?,?,?,?,?,?,?,?,?,?,?)
+    """, (age, sex, cp, trestbps,chol, fbs, restecg, thalach, exang, oldpeak, slope, ca))
+  
+
+    connection.commit()
+    connection.close()
+    return ""
+
+
+##########
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug= True)
